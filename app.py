77cc24d0ebed9131b5f1120e8a1f06c270fa7399
Kim-SuBin <sb2504.com>
2019-11-29 20:29:38 +0900

    Add requirements.txt

--- app.py
+++ app.py
@@ -30,15 +30,14 @@ def skill():
                         "description" : "Kakao Image",
                         "Thumbnail": {
                             "https://t1.daumcdn.net/friends/www/talk/kakaofriends_talk_2018.png"
-                        
-                            "buttons":[
-                                {
-                                    "label":"First button",
-                                    "action": "message",
-                                    "messageText" : "First button"
-                                }
-                            ]
-                        }
+                        },
+                        "buttons": [
+                            {
+                                "label":"First button",
+                                "action": "message",
+                                "messageText" : "First button"
+                            }
+                        ]
                     }
                     # "simpleImage":{
                     #     "imageUrl":
